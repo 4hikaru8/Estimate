6224ff0ae3162975f4fe67821cda8066d406346c
4hikaru8 <kotetu.iki@gmail.com>
2022-10-18 22:21:05 +0900

    アプリ設定をyml形式にした

--- src/main/java/com/bench_break/estimate/estimate/controllers/EstimateController.java
+++ src/main/java/com/bench_break/estimate/estimate/controllers/EstimateController.java
@@ -43,7 +43,7 @@ public class EstimateController {
         model.addAttribute("manufacturers", manufacturers);
         model.addAttribute("products", products);
 
-        log.info("estimate: {}", products);;
+        log.debug("estimate: {}", products);;
 
         return "estimate";
     }
